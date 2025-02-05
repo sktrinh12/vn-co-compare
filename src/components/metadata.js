19d41ba0a8fa37466aeefd817d700eedebe0a0cf
sktrinh12 <strkinh12@gmail.com>
2025-02-05 18:25:19 -0500

    remove un-used files

--- src/components/metadata.js
+++
@@ -1,5 +0,0 @@
-const imageUrl =
-  "https://shoutoutla.com/wp-content/uploads/2021/08/c-PersonalCindyTrinh__BBG0004996400072_1626393627487.jpg";
-const caption =
-  "Capturing the essence of the model minority, and telling stories through her lens.";
-export { caption, imageUrl };
