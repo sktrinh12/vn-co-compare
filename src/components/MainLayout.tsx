e8f72a3a5bb7f04704223f1bfa5f2bc6b5543711
sktrinh12 <strkinh12@gmail.com>
2025-02-08 14:02:36 -0500

    new main layout component to add header padding

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <Box sx={{ paddingTop: { xs: "64px", sm: "72px" }, padding: "0 22px" }}>
+      {children}
+    </Box>
+  );
+};
+
+export default MainLayout;
