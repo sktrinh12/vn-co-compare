7ce0bf0df70fa2a7c9b24a7f9a18e86985744cfe
sktrinh12 <strkinh12@gmail.com>
2025-02-08 14:01:58 -0500

    add new parqueadero data and update interface

--- src/components/images.ts
+++ src/components/images.ts
@@ -1,51 +1,99 @@
 export interface ImgDataInterface {
-  img: string;
-  title: string;
+  id: number;
+  city1: string;
+  city2: string;
+  img1: string;
+  img2: string;
+  category: string;
+  country1: string;
+  country2: string;
+  description1: string;
+  description2: string;
 }
 
 export const imgData: ImgDataInterface[] = [
   {
+    id: 1,
     city1: "Saigon",
-    city2: "BARRANQUILLA",
+    city2: "Barranquilla",
     country1: "VN",
     country2: "CO",
     img1:"https://afamilycdn.com/k:thumb_w/600/KIgpmlAko7AE3tMxv4rcRxIxxjDEe6/Image/2015/05/5-b530e/nhung-cay-cot-dien-moc-giua-duong-o-sai-gon.JPG",
     img2: "https://imagenes.eltiempo.com/files/image_1200_535/uploads/2019/01/26/5c4cebff6866a.jpeg",
-    title: "teleranas de cable",
-    description1: "TEST1",
-    description2: "TEST2",
+    category: "teleranas de cable",
+    description1: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    description2: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
   {
-    city1: "SAIGON",
-    city2: "MEDELLIN",
+    id: 2,
+    city1: "Saigon",
+    city2: "Medellin",
     country1: "VN",
     country2: "CO",
     img1: "https://afamilycdn.com/k:thumb_w/600/KIgpmlAko7AE3tMxv4rcRxIxxjDEe6/Image/2015/05/10-b530e/nhung-cay-cot-dien-moc-giua-duong-o-sai-gon.JPG",
     img2: "https://staticprd.minuto30.com/wp-content/uploads/2022/11/Cables-en-Medellin.jpg",
-    title: "teleranas de cable",
-    description1: "TEST1",
-    description2: "TEST2",
+    category: "teleranas de cable",
+    description1: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    description2: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
   {
-    city1: "SAIGON",
-    city2: "MEDELLIN",
+    id: 3,
+    city1: "Saigon",
+    city2: "Medellin",
     country1: "VN",
     country2: "CO",
     img1: "https://afamilycdn.com/k:thumb_w/600/KIgpmlAko7AE3tMxv4rcRxIxxjDEe6/Image/2015/05/1-b530e/nhung-cay-cot-dien-moc-giua-duong-o-sai-gon.JPG",
     img2: "https://www.epm.com.co/content/epm/institucional/sala-de-prensa/noticias-y-novedades/epm-desmontara-redes-de-telecomunicaciones-ilegales/_jcr_content/root/container/container_copy_copy_/news/container/tabs_copy/item_1/carousel/image.coreimg.jpeg/1721872098005/galeria-boletin-epm-desmontara-redes-de-telecomunicaciones-ilegales-companias-prestadoras-1.jpeg",
-    title: "teleranas de cable",
-    description1: "TEST1",
-    description2: "TEST2",
+    category: "teleranas de cable",
+    description1: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    description2: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
   {
-    city1: "MEDELLIN",
-    city2: "MANIZALES",
+    id: 4,
+    city1: "Medellin",
+    city2: "Manizales",
     country1: "CO",
     country2: "CO",
     img1: "https://estaticos.elcolombiano.com/binrepository/851x565/71c0/780d565/none/11101/EHWH/mcables-en-la-cuidad-1-3_40150189_20220615154411.jpg",
     img2: "https://www.lapatria.com/sites/default/files/styles/ampliar_945/public/noticia/2023-02/POSTES%20PRINCIPAL_0.jpg?itok=Wvi0mQni",
-    title: "teleranas de cable",
-    description1: "TEST1",
-    description2: "TEST2",
+    category: "teleranas de cable",
+    description1: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    description2: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+  {
+    id: 5,
+    city1: "Medellin",
+    city2: "Ha Noi",
+    country1: "CO",
+    country2: "VN",
+    img1: "https://eltesoro.com.co/wp-content/uploads/2021/04/0621-servicio-parqueadero-el-tesoro-motos-02.jpeg",
+    img2: "https://giaphaco.com/wp-content/uploads/2021/01/thi-cong-son-bai-dau-xe-may-1.jpg.webp",
+    category: "parqueaderos",
+    description1: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    description2: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+  {
+    id: 6,
+    city1: "Villavicencio",
+    city2: "Saigon",
+    country1: "CO",
+    country2: "VN",
+    img1: "https://unicentrovillavicencio.com/wp-content/uploads/600x400-motos.jpg",
+    img2: "https://hethonggiuxethongminhpth.com/wp-content/uploads/2021/06/tang-ham-bai-do-xe-may.jpg",
+    category: "parqueaderos",
+    description1: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    description2: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+  {
+    id: 7,
+    city1: "Bogota",
+    city2: "Saigon",
+    country1: "CO",
+    country2: "VN",
+    img1: "https://cloudfront-us-east-1.images.arcpublishing.com/semana/2NN4ZFU5RBBAHAQNMQESZ4BGVU.jpg",
+    img2: "https://bilparking.com.vn/sites/default/files/thu-vien/Baiviet/B%C3%A3i-%C4%91%E1%BA%ADu-xe-m%C3%A1y-chuy%C3%AAn-d%E1%BB%A5ng.jpg",
+    category: "parqueaderos",
+    description1: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    description2: "s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
 ];
